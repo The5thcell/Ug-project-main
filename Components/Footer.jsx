b38dcf5e477dc1bd7415a508d03afc05ccdd871c
The5thcell <dejisanu@gmail.com>
2024-03-31 13:21:08 +0100

    first commit

---
+++ Components/Footer.jsx
@@ -0,0 +1,46 @@
+export default () => {
+  const footerNavs = [
+    {
+      href: "javascript:void()",
+      name: "Terms",
+    },
+    {
+      href: "javascript:void()",
+      name: "License",
+    },
+    {
+      href: "javascript:void()",
+      name: "Privacy",
+    },
+    {
+      href: "javascript:void()",
+      name: "About us",
+    },
+  ];
+  return (
+    <footer className="pt-10">
+      <div className="max-w-screen-xl mx-auto px-4 text-gray-600 md:px-8">
+        <div className="justify-between sm:flex">
+          <div className="space-y-6">
+            <ul className="flex flex-wrap items-center gap-4 text-sm sm:text-base">
+              {footerNavs.map((item, idx) => (
+                <li className="text-gray-800 hover:text-gray-500 duration-150">
+                  <a key={idx} href={item.href}>
+                    {item.name}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="mt-6">
+            <p className="text-gray-700 font-semibold">Trace the journey of your product from origin to destination with our blockchain-powered supply chain solution.</p>
+  
+          </div>
+        </div>
+        <div className="mt-10 py-10 border-t md:text-center">
+          <p>© 2024 Sanusi Adedeji. All rights reserved.</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
---
+++ Components/GetShipment.jsx
@@ -0,0 +1,25 @@
+
+
+export default () => {
+  
+  return (
+    <footer className="pt-10">
+      <div className="max-w-screen-xl mx-auto px-4 text-gray-600 md:px-8">
+        <div className="justify-between sm:flex">
+          <div className="space-y-6">
+           
+          </div>
+          <div className="mt-6">
+            <p className="text-gray-700 font-semibold">Get the app</p>
+            <div className="flex items-center gap-3 mt-3 sm:block">
+           
+            </div>
+          </div>
+        </div>
+        <div className="mt-10 py-10 border-t md:text-center">
+          <p>© 2024 Sanusi Adedeji. All rights reserved.</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
